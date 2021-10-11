d8111767f758cc614b2f89750543f9efb223d001
Brian Kaplan <brian@softwaredeployed.com>
2021-10-11 13:59:10 -0500

    initial bootstrap + minimal config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+extern crate rocket_contrib;
+
+use rocket_sync_db_pools::{diesel, database};
+
+#[database("postgres")]
+struct MainDbConn(diesel::PgConnection);
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().attach(MainDbConn::fairing())
 }
